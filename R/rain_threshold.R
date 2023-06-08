a8eb6d225b092d67a8c83710aec529a5134d4bc5
PaulMelloy <paul@melloy.com.au>
2023-06-08 14:31:58 +1000

    init function

---
+++ R/rain_threshold.R
@@ -0,0 +1,49 @@
+#' Evaluate rainfall threshold
+#'
+#' @param w data.table
+#' @param days integer, time period in days for which the rainfall threshold is
+#'   calculated
+#' @param rain_mm numeric, total rainfall needed in time period (`days`) for rainfall
+#'   threshold to be met.
+#'
+#' @return A `epiphy.weather` object (an extension of \CRANpkg{data.table}) with
+#'   an additional logical column `rain_threshold`.  Read more at `?(format_weather)`
+#'   The added `rain_threshold` indicates whether the rainfall threshold was met.
+#' @import data.table
+#' @export
+#' @examples
+#' scaddan <-
+#'    system.file("extdata", "scaddan_weather.csv",package = "epiphytoolR")
+#' weather_dat <- read.csv(scaddan)
+#' weather_dat$Local.Time <-
+#'    as.POSIXct(weather_dat$Local.Time, format = "%Y-%m-%d %H:%M:%S",
+#'               tz = "UTC")
+#'
+#' weather <- format_weather(
+#'    w = weather_station_data,
+#'    POSIXct_time = "Local.Time",
+#'    ws = "meanWindSpeeds",
+#'    wd_sd = "stdDevWindDirections",
+#'    rain = "Rainfall",
+#'    temp = "Temperature",
+#'    wd = "meanWindDirections",
+#'    lon = "Station.Longitude",
+#'    lat = "Station.Latitude",
+#'    station = "StationID",
+#'    time_zone = "UTC"
+#' )
+#'
+#' weather_out <- rain_threshold(weather)
+#' weather_out
+rain_threshold <- function(w,
+                           days = 2,
+                           rain_mm = 2) {
+  w[,rain_threshold := fifelse(frollsum(rain,
+                                        days,
+                                        align = "right",
+                                        na.rm = TRUE) ==
+                                 rain_mm,
+                               TRUE,FALSE)]
+
+  return(w)
+}
