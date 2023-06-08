07e9de89b857357e5bdf1daba5e7ae193648c0af
PaulMelloy <paul@melloy.com.au>
2023-06-08 18:50:00 +1000

    specify packages from imports

--- R/cercospora_dk.R
+++ R/cercospora_dk.R
@@ -4,10 +4,10 @@ cercospora_dk <- function(x, y, sigma, lambda) {
   # lambda is the parameter controlling the exponential decay component
 
   # Calculate the Gaussian component
-  gaussian <- dnorm(x, mean = 0, sd = sigma) * dnorm(y, mean = 0, sd = sigma)
+  gaussian <- stats::dnorm(x, mean = 0, sd = sigma) * dnorm(y, mean = 0, sd = sigma)
 
   # Calculate the exponential component
-  exponential <- dexp(sqrt(x^2 + y^2), rate = lambda)
+  exponential <- stats::dexp(sqrt(x^2 + y^2), rate = lambda)
 
   # Combine the two components to get the final dispersal kernel
   return(gaussian * exponential)
