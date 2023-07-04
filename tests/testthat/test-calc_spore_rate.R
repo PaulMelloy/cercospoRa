47ef4b4ac1509c45889713179c71fc965f8f7b82
PaulMelloy <paul@melloy.com.au>
2023-07-04 16:30:20 +1000

    add test

---
+++ tests/testthat/test-calc_spore_rate.R
@@ -0,0 +1,15 @@
+test_that("infection rate returns sensible values", {
+  expect_equal(calc_spore_rate(25,98),0.6115753, tolerance = 0.000001)
+  expect_equal(calc_spore_rate(30,94),0.01922715, tolerance = 0.000001)
+
+  temp <- seq(-5,55, by = 0.5)
+  RH <- seq(0,100, by = 0.5)
+  s_rate <-
+    outer(temp,RH,function(xi,ji){
+      calc_spore_rate(Tm = xi, RH = ji)
+    })
+
+  expect_true(all(s_rate < 1 &
+                    s_rate >= 0 ))
+#  persp(temp,RH,s_rate, theta = 160)
+})
