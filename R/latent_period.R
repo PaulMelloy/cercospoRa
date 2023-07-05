21ded2530dd6d0e4f602da52fcafdda88b194af3
PaulMelloy <paul@melloy.com.au>
2023-07-06 07:48:24 +1000

    add temperature and humidity index functions

--- R/latent_period.R
+++ R/latent_period.R
@@ -3,11 +3,59 @@
 #' @param Tm numeric, Average temperature in degrees Celsius for a hour of the day
 #'
 #' @return numeric, Latent period for a given hour
-#' @references \insertRef{@wolf_factors_2005}{cercosporaR} equation 4 (Wolf 2001)
+#' @references \insertRef{wolf_factors_2005}{cercosporaR}; equation 4
 #'
-#' @examples
-#' latent_period(25)
 latent_period <- function(Tm){
   lp <- 7 + 26 * exp(-0.35*(Tm - 10))
+
   return(lp)
+}
+
+
+
+#' Get temperature index
+#'
+#' Temperature index is a proportional representation of the latent period.
+#' Temperatures at or above the optimum temperature for the disease cycle will
+#' yield 1 and the lower the temperature the infinitely longer the latent period.
+#'
+#' For equations and original documentation, see \insertCite{wolf_factors_2005}{cercosporaR}
+#'  and \insertCite{wolf_zum_2001}{cercosporaR}
+#'
+#' @param Tm numeric, temperature for any given hour
+#' @param opt_Tm numeric, the lowest temperature optimum at which all temperatures
+#'  above will have the same (fastest) latent period.
+#'
+#' @return numeric, proportion representing the speed of the latent period in relation
+#'  to the temperature optimum
+#' @importFrom Rdpack reprompt
+#' @references
+#'    \insertAllCited{}
+temperature_index <- function(Tm, opt_Tm = 23){
+
+  tm_ind <- latent_period(opt_Tm)/latent_period(Tm)
+  if(tm_ind > 1) tm_ind <- 1
+  if(tm_ind < 0) tm_ind <- 0
+
+  return(tm_ind)
+}
+
+
+#' Relitive humidity index
+#'
+#' Confirms the humidity threshold has been met by returning a 1 or not met by
+#'  returning a 0. See \insertCite{wolf_factors_2005}{cercosporaR}
+#'
+#' @param RH numeric, relative humidity as a percentage
+#' @param rh_thresh numeric, humidity threshold where if humidity is below this
+#'  no progress towards the latent period is made.
+#'
+#' @return numeric, either a 1 or 0
+#' @references
+#'    \insertAllCited{}
+moisture_index <- function(RH, rain = 0, rh_thresh = 90){
+  rh_ind <- ifelse(RH > rh_thresh |
+                     rain >= 0.1 ,1,0)
+
+  return(rh_ind)
 }
