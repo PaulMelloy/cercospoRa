926f69b0c16c211398f7f4ed4efcfc95b3db9996
PaulMelloy <paul@melloy.com.au>
2023-07-05 17:24:06 +1000

    Correct function based on Wolf 2001 paper

--- R/latent_period.R
+++ R/latent_period.R
@@ -1,12 +1,13 @@
 #' Calculate Cercospora latent period
 #'
-#' @param Tm numeric, Average temperature in degrees celcius for a hour of the day
+#' @param Tm numeric, Average temperature in degrees Celsius for a hour of the day
 #'
 #' @return numeric, Latent period for a given hour
-#' @references \insertRef{@wolf_factors_2005}{cercosporaR} equation 4
+#' @references \insertRef{@wolf_factors_2005}{cercosporaR} equation 4 (Wolf 2001)
 #'
 #' @examples
+#' latent_period(25)
 latent_period <- function(Tm){
-  lp <-7 + 26 * exp(0.35*(Tm - 10))
+  lp <- 7 + 26 * exp(-0.35*(Tm - 10))
   return(lp)
 }
