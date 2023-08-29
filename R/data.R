078a49526da146511ccffd5832a29aa53d07cf1c
PaulMelloy <paul@melloy.com.au>
2023-08-30 04:31:57 +1000

    define dataset

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Weather station data
+#'
+#' A unformatted weather station dataset from Germany
+#'
+#' @format ## `weathr`
+#' A data frame with 8,016 rows and 15 columns:
+#' \describe{
+#'   \item{Station}{Identifier}
+#'   \item{Datum}{Date}
+#'   \item{Stunde}{hour}
+#'   \item{Minute}{minute}
+#'   \item{T-005}{Temperature at 5cm}
+#'   \item{T-200}{Temperature at 200cm}
+#'   \item{T-020}{Temperature at 20cm}
+#'   \item{F-200}{Relative humidity at 200cm}
+#'   ...
+#' }
+#' @source Facundo
+
