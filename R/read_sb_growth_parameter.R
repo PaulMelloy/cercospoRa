396a6bbb9540737fb3a146b1c942e1ae10a6a61d
Nathan Okole <nathan.karume@stud.uni-goettingen.de>
2024-03-14 01:31:53 +0100

    minor edits

--- R/read_sb_growth_parameter.R
+++ R/read_sb_growth_parameter.R
@@ -14,7 +14,7 @@
 #' @import epiphytoolR
 #'
 #' @examples
-#' epidemic_onset_param <- read_sb_growth_parameter(system.file("extdata", "uav_img", package = "your_package_name"), 10)
+#' epidemic_onset_param <- read_sb_growth_parameter(system.file("extdata", "uav_img", package = "cercospoRa"), 10)
 #' t <- epidemic_onset_param$t
 #' imgs <- epidemic_onset_param$imgs
 
