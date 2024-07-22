22dcb279ab938fa8f9907625d676698d6874005f
PaulMelloy <paul@melloy.com.au>
2024-07-22 21:21:26 +1000

    Threshold updated effect on expected values

--- tests/testthat/test-calc_DIV.R
+++ tests/testthat/test-calc_DIV.R
@@ -6,7 +6,7 @@ test_that("calc_DIV works", {
     RH = RH1,
     rain = rain1
   )
-  expect_equal(DIV1$DIV, c(0.8277005844, 0.2345840374 ,0.0009688822), tolerance = 0.000001)
+  expect_equal(DIV1$DIV, c(0.8445079, 0.8196010 ,0.8513760), tolerance = 0.00001)
   expect_type(DIV1,"list")
   expect_equal(dim(DIV1), c(3,4))
   expect_equal(colnames(DIV1), c("Year","Month","Day","DIV"))
@@ -27,17 +27,17 @@ test_that("calc_DIV works", {
     rh = RH1,
     rain = rain1
   )
-  calc_DIV(dat = in_dat)
+  #calc_DIV(dat = in_dat)
 
 })
 
 # define DIV calculation
 
-dat <- data.table(Tm = rep(0:49, times = 50),
-                  Rh = rep (51:100, each = 50))
-
-dat[, DIV := list(temperature_index(Tm)*
-                    moisture_index(Rh))]
+# dat <- data.table(Tm = rep(0:49, times = 50),
+#                   Rh = rep (51:100, each = 50))
+#
+# dat[, DIV := list(temperature_index(Tm)*
+#                     moisture_index(Rh))]
 # library(ggplot2)
 # ggplot(dat, aes(x = Tm, y = Rh, z = DIV))+
 #   geom_contour_filled()
