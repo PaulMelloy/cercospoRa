5c612e02e5e98328e6e13fb5392ac527a25949f2
PaulMelloy <paul@melloy.com.au>
2024-07-25 09:35:51 +1000

    Remove loop and use terra::app #19

--- R/calc_r_x0.R
+++ R/calc_r_x0.R
@@ -2,12 +2,14 @@
 #'
 #' @param param_r Output of the function \\link{read_sb_growth_parameter}, which
 #'  produces a list containing the LAI images and the associated dates
-#' @param min_r minimum growth rate for sugar beet. Default `min_r` is fixed to 0.02 to ensure that the growth rate at the
-#' inflexion point of the sigmoid is at least 1 unit of LAI per month
-#' @param max_r manimum growth rate for sugar beet. Default `max_r` is fixed to 0.05 to ensure that the growth rate at the
-#' inflexion point of the sigmoid is at most 2.5 units of LAI per month
-#' @param k carrying capaciy, which is the maximum LAI that can be attained. This value can be cultivar-dependent. The default
-#' is fixed to 6
+#' @param min_r minimum growth rate for sugar beet. Default `min_r` is fixed to
+#'  0.02 to ensure that the growth rate at the inflexion point of the sigmiod is
+#'  at least 1 unit of LAI per month.
+#' @param max_r manimum growth rate for sugar beet. Default `max_r` is fixed to
+#'  0.05 to ensure that the growth rate at the inflexion point of the sigmoid is
+#'  at most 2.5 units of LAI per month.
+#' @param k carrying capaciy, which is the maximum LAI that can be attained.
+#'  This value can be cultivar-dependent. The default is fixed to 6
 #'
 #' @return `param_rxt:` list containing parameters that are necessary to calculate `c_closure`. These parameters are `r`, the
 #' growth rate, `x0`, the initial LAI value, and `t0`, the initial date.
@@ -15,9 +17,13 @@
 #' @export
 #'
 #' @examples
-#' epidemic_onset_param <- read_sb_growth_parameter(system.file("extdata", "uav_img",
-#'                                                              package = "cercospoRa"),
-#'                                                  10)
+#' img_dir <- system.file("extdata", "uav_img",package = "cercospoRa")
+#' epidemic_onset_param <-
+#'   read_sb_growth_parameter(img_files = list.files(img_dir,pattern = "tif",
+#'                                                   full.names = TRUE),
+#'                            img_dates = as.POSIXct(
+#'                              c("2022-06-14","2022-06-28"),tz = "UTC"),
+#'                            target_res = 10)
 #' param_rxt <- calc_r_x0(epidemic_onset_param,
 #'                        min_r = 0.02,
 #'                        max_r = 0.05,
@@ -30,37 +36,8 @@ calc_r_x0 <- function(param_r,
   tm <- as.numeric(tm)/(24*60*60)
   t0 <- tm[1]
 
-  r <- param_r[[1]]
-  x0 <- param_r[[1]]
-
-
-  for(i in 1:dim(param_r)[1]){
-    for(j in 1:dim(param_r)[2]){
-      N <- as.numeric(param_r[i,j,])
-      dataij <- data.frame(tm, N)
-      dataij <- stats::na.omit(dataij)
-
-      if(!is.na(N[1])){
-        if(nrow(dataij) == 1){
-          r[i,j] <- min_r
-        }else if (nrow(dataij) > 1){
-          fit_rx <-
-            minpack.lm::nlsLM(N ~ k/(1 + ((k-x_ij)/x_ij)*exp(-r_ij*(tm-t0))),
-                              start = list(x_ij = 1,
-                                           r_ij = 0.025),
-                              lower = c(1e-6,
-                                        1e-6),
-                              algorithm = "port",
-                              data = dataij)
-          x_ij <- fit_rx$m$getAllPars()[1]
-          r_ij <- fit_rx$m$getAllPars()[2]
-
-          r[i,j] <- r_ij
-          x0[i,j] <- x_ij
-        }
-      }
-    }
-  }
+  x0 <- terra::app(param_r, fit_rx, k = k, tm = tm, t0 = t0, rtn = "x0")
+  r <- terra::app(param_r, fit_rx, k = k, tm = tm, t0 = t0, rtn = "r")
 
   try(r[r<min_r] <- min_r)
   try(r[r>=max_r] <- max_r)
@@ -69,4 +46,35 @@ calc_r_x0 <- function(param_r,
                     x0=x0,
                     t0=names(param_r)[1])
   return(param_rxt)
+}
+
+
+#' Fit non-linear model to raster layers
+#'
+#' @param xyi Spatrast stack
+#' @param k carrying capaciy, which is the maximum LAI that can be attained.
+#'  This value can be cultivar-dependent. The default is fixed to 6
+#' @param tm numeric, form of dates from each raster layer in `xyi`
+#' @param t0 numeric, form of date from first raster layer in `xyi`
+#' @param rtn character, either "x0" for the fitted value of the first layer or
+#'  "r" for the estimated value.
+#'
+#' @return SpatRast, see `rtn`
+#' @noRd
+fit_rx <- function(xyi, k, tm,t0,rtn){
+  if(any(is.na(xyi))) return(NA_real_)
+  dataij <- data.frame(Tm = tm, XYi = xyi)
+  fitted_rx <-
+    minpack.lm::nlsLM(XYi ~ k/(1 + ((k-x_ij)/x_ij)*exp(-r_ij*(Tm-t0))),
+                      start = list(x_ij = 1,
+                                   r_ij = 0.025),
+                      lower = c(1e-6,
+                                1e-6),
+                      algorithm = "port",
+                      data = dataij)
+  if(rtn == "x0") return(fitted_rx$m$getAllPars()[1])
+  if(rtn == "r") return(fitted_rx$m$getAllPars()[2])else(
+    stop("rtn must be 'x0' or 'r'")
+  )
+
 }
