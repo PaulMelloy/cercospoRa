5f0c7e7560cdbea47e4b3f7d35b0268e7c409274
PaulMelloy <paul@melloy.com.au>
2024-12-04 22:19:56 +1000

    update missing internal objects - FAILING

--- tests/testthat/test-calc_DIV.R
+++ tests/testthat/test-calc_DIV.R
@@ -1,3 +1,8 @@
+date_t <- unique(as.POSIXct(weathr$Datum))[c(1,2)]
+Tm1 <- weathr$T200[c(1,288)]
+RH1 <- weathr$F200[c(1,288)]
+rain1 <- c(0,5)
+
 test_that("calc_DIV works", {
   # individual vectors
   DIV1 <- calc_DIV(
@@ -6,7 +11,7 @@ test_that("calc_DIV works", {
     RH = RH1,
     rain = rain1
   )
-  expect_equal(DIV1$DIV, c(0.7845169, 0.8551818), tolerance = 0.00001)
+  expect_equal(DIV1$DIV, c(0.4373111, 0.0214606), tolerance = 0.00001)
   expect_type(DIV1,"list")
   expect_equal(dim(DIV1), c(2,4))
   expect_equal(colnames(DIV1), c("Year","Month","Day","DIV"))
@@ -30,7 +35,7 @@ test_that("calc_DIV works", {
   )
   dat_out <- calc_DIV(dat = in_dat)
 
-  expect_equal(dat_out$DIV, c(0.7845169, 0.8551818), tolerance = 0.00001)
+  expect_equal(dat_out$DIV, c(0.4373111, 0.0214606), tolerance = 0.00001)
   expect_type(dat_out,"list")
   expect_equal(dim(dat_out), c(2,4))
   expect_equal(colnames(dat_out), c("Year","Month","Day","DIV"))
