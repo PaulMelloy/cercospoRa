b926f6c4994f68806b37f031461105b7bde9e087
PaulMelloy <paul@melloy.com.au>
2025-05-11 09:30:37 +1000

    fix bugs, add header

--- R/interpret_gam.R
+++ R/interpret_gam.R
@@ -1,25 +1,44 @@
+#' Interpret Generalised Additive Model Output
+#'
+#' @param mod model output of class `gam`
+#' @param words integer, indicating different contrasting words. 0 = the numerical
+#'  output; 1 = 'more' or 'less'; 2 = 'greater' or 'less'; 3 = 'higher' or 'lower'
+#' @param rounding integer, indicating the number of decimals
+#' @param coeff character, specifyinf the coeficcint name as it appears in the
+#'  model output. Defaults to "(Intercept)".
+#' @param sig logical, if TRUE return the statistical significance associated
+#'  with the `coeff`. words = 1 returns "not significant" or "significant".
+#'  `words = 0` returns the numeric output.
+#'
+#' @returns character or numeric depending on `words`
+#' @export
+#'
+#' @examples
+#' mod1 <- lm(speed ~ dist, data = cars)
+#' interpret
 interpret_gam <- function(mod,
                           words = 1,
                           rounding = 2,
                           coeff = "(Intercept)",
                           sig = FALSE) {
 
+  words <- as.character(words)
+
   if(sig){
     out <- switch(words,
-                  0 = round(unname(summary(mod)$p.table[,4][coeff]),rounding),
-                  1 = ifelse(summary(mod)$p.table[,4][coeff] >0.05,
+                  "0" = round(unname(summary(mod)$p.table[,4][coeff]),rounding),
+                  "1" = ifelse(summary(mod)$p.table[,4][coeff] >0.05,
                              "not significant",
                              "significant"))
-    }
-  }else{
+    }else{
 
   out <- switch(words,
-                0 = round(unname(mod$coefficients[coeff]),rounding),
-                1 = ifelse(mod$coefficients[coeff] > 0, "more", "less"),
-                2 = ifelse(mod$coefficients[coeff] > 0, "greater", "fewer"),
-                3 = ifelse(mod$coefficients[coeff] > 0, "higher", "lower"))
+                "0" = round(unname(mod$coefficients[coeff]),rounding),
+                "1" = ifelse(mod$coefficients[coeff] > 0, "more", "less"),
+                "2" = ifelse(mod$coefficients[coeff] > 0, "greater", "fewer"),
+                "3" = ifelse(mod$coefficients[coeff] > 0, "higher", "lower"))
   }
 
-  return(out)
+  return(unname(out))
 
 }
